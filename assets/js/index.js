5f042fed1de839f8e088894fab9fac408a56f5d6
Guille Cura <cura.gf@gmail.com>
2016-05-31 00:06:01 -0300

    Added sticky menu js.

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,6 +13,7 @@
       var $postContent = $(".post-content");
       $postContent.fitVids();
 
+      // Menu & menu button animation
       $(".menu-button").on("click", function(e){
         e.preventDefault();
 
@@ -28,6 +29,45 @@
           $("body").toggleClass("nav-opened nav-closed");
           $("body > .nav").addClass("closed");
 
+        }
+      });
+
+      // Sticky navs
+      var $tagAside = $(".tag-aside");
+      var $tagAsideSibling = $tagAside.next(".tag-content");
+
+      var $aboutMenu = $(".about-content .menu-box");
+
+      var $postAside = $(".post-template .content > aside");
+
+      var headerHeight = $("nav.main-nav").height();
+      var aboutHeight = $(".author-profile").height();
+      var postHeight = $(".post-heading").height();
+
+      $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        // Post lists / tag page
+        if (scroll >= headerHeight) {
+          $tagAside.addClass("fixed");
+          $tagAsideSibling.addClass("fixed");
+        } else {
+          $tagAside.removeClass("fixed");
+          $tagAsideSibling.removeClass("fixed");
+        }
+
+        // About page
+        if (scroll >= aboutHeight+headerHeight+80) {
+          $aboutMenu.addClass("fixed");
+        } else {
+          $aboutMenu.removeClass("fixed");
+        }
+
+        // Post page
+        if (scroll >= postHeight+headerHeight+80) {
+          $postAside.addClass("fixed");
+        } else {
+          $postAside.removeClass("fixed");
         }
       });
     });
